d966365cc815a29fe5289973b5bd021a7e17b987
Keziah Biermann <development@kizzycode.de>
2022-09-06 03:23:50 +0200

    Fix bindgen in CI

--- evercrypt_tiny-sys/build/main.rs
+++ evercrypt_tiny-sys/build/main.rs
@@ -8,10 +8,12 @@ use make::Make;
 
 fn main() {
     // Build the library
+    println!("Building library...");
     let config = Configuration::new();
     let make = Make::new(config);
     make.build();
 
     // Generate bindings
+    println!("Generating bindings...");
     bindgen::generate();
 }
